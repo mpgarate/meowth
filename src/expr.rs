09eb8603ea1ff7977d035c1832fea5498a447dea
michael <mpgarate@gmail.com>
2016-07-04 21:36:25 -0400

    subtraction

--- src/expr.rs
+++ src/expr.rs
@@ -24,16 +24,20 @@ impl Expr {
 
 fn eval(e: Expr) -> Expr {
   match e {
-    Expr::BinOp(Op::Plus, v1, v2) => {
-      match (*v1, *v2) {
+    Expr::BinOp(Op::Plus, e1, e2) => {
+      let v1 = eval(*e1);
+      let v2 = eval(*e2);
+      match (v1, v2) {
         (Expr::Integer(n1), Expr::Integer(n2)) => {
           Expr::Integer(n1 + n2)
         }
         _ => panic!()
       }
     },
-    Expr::BinOp(Op::Minus, v1, v2) => {
-      match (*v1, *v2) {
+    Expr::BinOp(Op::Minus, e1, e2) => {
+      let v1 = eval(*e1);
+      let v2 = eval(*e2);
+      match (v1, v2) {
         (Expr::Integer(n1), Expr::Integer(n2)) => {
           Expr::Integer(n1 - n2)
         }
@@ -41,5 +45,20 @@ fn eval(e: Expr) -> Expr {
       }
     },
     Expr::Integer(n) => Expr::Integer(n),
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use parser::parse;
+  use super::{Expr, eval};
+
+  #[test]
+  pub fn test_eval_addition() {
+    assert_eq!(Expr::Integer(3), eval(parse("1+2")));
+    assert_eq!(Expr::Integer(16), eval(parse("5+7+4")));
+    assert_eq!(Expr::Integer(-1), eval(parse("1-2")));
+    assert_eq!(Expr::Integer(-100), eval(parse("32-132")));
+    assert_eq!(Expr::Integer(-120), eval(parse("32-132-20")));
   }
 }
