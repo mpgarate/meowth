cb313f7733a17e5d0b01d4d94ffaef0cd078b8b9
michael <mpgarate@gmail.com>
2016-09-05 15:22:04 -0700

    pull expr into repl instance

--- src/expr.rs
+++ src/expr.rs
@@ -69,216 +69,233 @@ fn subst(e: Expr, x: Expr, v: Expr) -> Expr {
   }
 }
 
-pub fn step(mut state: State) -> State {
-  let st_step = |s: &mut State, e1: &Expr| {
-    let s2 = step(s.clone().with(e1.clone()));
-    s.merge_mem(s2.clone());
+pub struct Repl {
+  pub state: State,
+}
+
+impl Repl {
+  pub fn new(e: Expr) -> Repl {
+    Repl {
+      state: State::from(e),
+    }
+  }
+
+  fn st_step(&mut self, e1: &Expr) -> Expr {
+    let s2 = self.step(e1.clone());
+    self.state.merge_mem(s2.clone());
     s2.expr.clone()
-  };
+  }
 
-  debug!("step(e) : {:?}", state.expr);
-  debug!("step(state) : {:?}", state.mem);
-  let e1 = match state.expr.clone() {
-    Var(x) => {
-      state.get(x)
-    },
-    /**
-     * Values are ineligible for step
-     */
-    Int(_) | Bool(_) | Func(_, _, _) | Undefined => {
-      debug!("stepping on a value {:?}", state.expr);
-      panic!("stepping on a value");
-    },
-    /**
-     * Base cases
-     */
-    Uop(Not, ref e1) if e1.is_bool() => {
-      Bool(!e1.to_bool())
-    },
-    Uop(Neg, ref e1) if e1.is_int() => {
-      Int(-1 * e1.to_int())
-    },
-    Bop(And, ref e1, ref e2) if e1.is_bool() && e2.is_bool() => {
-      Bool(e1.to_bool() && e2.to_bool())
-    },
-    Bop(Or, ref e1, ref e2) if e1.is_bool() && e2.is_bool() => {
-      Bool(e1.to_bool() || e2.to_bool())
-    },
-    Bop(Eq, ref e1, ref e2) if e1.is_value() && e2.is_value() => {
-      Bool(*e1 == *e2)
-    },
-    Bop(Ne, ref e1, ref e2) if e1.is_value() && e2.is_value() => {
-      Bool(*e1 != *e2)
-    },
-    Bop(Mod, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
-      let n1 = e1.to_int();
-      let n2 = e2.to_int();
+  pub fn step(&mut self, e: Expr) -> State {
+    debug!("step(e) : {:?}", self.state.expr);
+    debug!("step(self.state) : {:?}", self.state.mem);
 
-      // rust % gives the remainder, not modulus
-      let result = ((n1 % n2) + n2) % n2;
+    let e1 = match e.clone() {
+      Var(x) => {
+        self.state.get(x)
+      },
+      /**
+       * Values are ineligible for step
+       */
+      Int(_) | Bool(_) | Func(_, _, _) | Undefined => {
+        debug!("stepping on a value {:?}", self.state.expr);
+        panic!("stepping on a value");
+      },
+      /**
+       * Base cases
+       */
+      Uop(Not, ref e1) if e1.is_bool() => {
+        Bool(!e1.to_bool())
+      },
+      Uop(Neg, ref e1) if e1.is_int() => {
+        Int(-1 * e1.to_int())
+      },
+      Bop(And, ref e1, ref e2) if e1.is_bool() && e2.is_bool() => {
+        Bool(e1.to_bool() && e2.to_bool())
+      },
+      Bop(Or, ref e1, ref e2) if e1.is_bool() && e2.is_bool() => {
+        Bool(e1.to_bool() || e2.to_bool())
+      },
+      Bop(Eq, ref e1, ref e2) if e1.is_value() && e2.is_value() => {
+        Bool(*e1 == *e2)
+      },
+      Bop(Ne, ref e1, ref e2) if e1.is_value() && e2.is_value() => {
+        Bool(*e1 != *e2)
+      },
+      Bop(Mod, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
+        let n1 = e1.to_int();
+        let n2 = e2.to_int();
 
-      Int(result)
-    },
-    Bop(Lt, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
-      Bool(e1.to_int() < e2.to_int())
-    },
-    Bop(Gt, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
-      Bool(e1.to_int() > e2.to_int())
-    },
-    Bop(Leq, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
-      Bool(e1.to_int() <= e2.to_int())
-    },
-    Bop(Geq, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
-      Bool(e1.to_int() >= e2.to_int())
-    },
-    Bop(Plus, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
-      Int(e1.to_int() + e2.to_int())
-    },
-    Bop(Minus, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
-      Int(e1.to_int() - e2.to_int())
-    },
-    Bop(Times, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
-      Int(e1.to_int() * e2.to_int())
-    },
-    Bop(Div, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
-      Int(e1.to_int() / e2.to_int())
-    },
-    Bop(Seq, ref v1, ref e2) if v1.is_value() => {
-      *e2.clone()
-    },
-    Bop(Assign, ref v1, ref v2) if v1.is_var() && v2.is_value() => {
-      let x = v1.to_var();
-      state.assign(x, *v2.clone());
-      debug!("done assigning {:?}", state.mem);
-      *v2.clone()
-    },
-    Ternary(ref v1, ref e2, ref e3) if v1.is_value() => {
-      match v1.to_bool() {
-        true => *e2.clone(),
-        false => *e3.clone(),
-      }
-    },
-    Decl(DConst, ref x, ref v1, ref e2) if v1.is_value() => {
-      subst(*e2.clone(), *x.clone(), *v1.clone())
-    },
-    Decl(DVar, ref x, ref v1, ref e2) if x.is_var() && v1.is_value() => {
-      debug!("allocing {:?}", v1);
-      state.alloc(x.to_var(), *v1.clone());
-      Scope(Box::new(*e2.clone()), x.to_var())
-    },
-    Scope(ref v1, ref x) if v1.is_value() => {
-      debug!("freeing {:?}", x);
-      state.free(x.clone());
-      *v1.clone()
-    },
-    // lambda lift so we can use iter() in guard
-    // https://github.com/rust-lang/rfcs/issues/1006
-    FnCall(ref v1, ref es) if v1.is_func() && (|| es.iter().all(|v| v.is_value()))() => {
-      match **v1 {
-        Func(ref name, ref e1, ref xs) => {
-          // sub the params
-          let exp = xs.iter().zip(es.iter())
-            .fold(*e1.clone(), |exp, (xn, en)| subst(exp, xn.clone(), en.clone()));
+        // rust % gives the remainder, not modulus
+        let result = ((n1 % n2) + n2) % n2;
 
-          // sub the fn body for named functions
-          match *name {
-            None => exp,
-            Some(ref s) => subst(exp, *s.clone(), *v1.clone())
-          }
-        },
-        _ => {
-          debug!("expected a Func, got {:?}", v1);
-          panic!()
-        },
-      }
-    },
-    /**
-     * Search Cases
-     */
-    Bop(ref op, ref v1, ref e2) if v1.is_value() => {
-      Bop(
-        op.clone(),
-        Box::new(*v1.clone()),
-        Box::new(st_step(&mut state, e2))
-      )
-    },
-    Bop(Assign, ref v1, ref e2) if v1.is_var() => {
-      Bop(
-        Assign,
-        Box::new(*v1.clone()),
-        Box::new(st_step(&mut state, e2))
-      )
-    },
-    Bop(op, e1, e2) => {
-      Bop(op, Box::new(st_step(&mut state, &*e1)), e2)
-    },
-    Scope(e1, addr) => {
-      Scope(Box::new(st_step(&mut state, &*e1)), addr)
-    },
-    Uop(op, e1) => {
-      Uop(op, Box::new(st_step(&mut state, &*e1)))
-    },
-    Ternary(e1, e2, e3) => {
-      Ternary(Box::new(st_step(&mut state, &*e1)), e2, e3)
-    },
-    While(ref v1, ref e1o, _, ref e2o, ref e3) if v1.is_value() => {
-      match v1.to_bool() {
-        true => While(Box::new(*e1o.clone()), e1o.clone(), e2o.clone(), e2o.clone(), e3.clone()),
-        false => *e3.clone(),
-      }
-    },
-    While(ref e1, ref e1o, ref v2, ref e2o, ref e3) if v2.is_value() => {
-      While(Box::new(st_step(&mut state, &*e1)), e1o.clone(), v2.clone(), e2o.clone(), e3.clone())
-    },
-    While(e1, e1o, e2, e2o, e3) => {
-      While(e1, e1o, Box::new(st_step(&mut state, &*e2)), e2o, e3)
-    },
-    Decl(dt, addr, e1, e2) => {
-      Decl(dt, Box::new(*addr.clone()), Box::new(st_step(&mut state, &*e1)), e2)
-    },
-    FnCall(ref v1, ref args) if v1.is_value() => {
-      let mut found_nonvalue = false;
-
-      let args2 = args.iter().map(|e| {
-        if !found_nonvalue && !e.is_value() {
-          found_nonvalue = true;
-          st_step(&mut state, e)
-        } else {
-          e.clone()
+        Int(result)
+      },
+      Bop(Lt, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
+        Bool(e1.to_int() < e2.to_int())
+      },
+      Bop(Gt, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
+        Bool(e1.to_int() > e2.to_int())
+      },
+      Bop(Leq, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
+        Bool(e1.to_int() <= e2.to_int())
+      },
+      Bop(Geq, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
+        Bool(e1.to_int() >= e2.to_int())
+      },
+      Bop(Plus, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
+        Int(e1.to_int() + e2.to_int())
+      },
+      Bop(Minus, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
+        Int(e1.to_int() - e2.to_int())
+      },
+      Bop(Times, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
+        Int(e1.to_int() * e2.to_int())
+      },
+      Bop(Div, ref e1, ref e2) if e1.is_int() && e2.is_int() => {
+        Int(e1.to_int() / e2.to_int())
+      },
+      Bop(Seq, ref v1, ref e2) if v1.is_value() => {
+        *e2.clone()
+      },
+      Bop(Assign, ref v1, ref v2) if v1.is_var() && v2.is_value() => {
+        let x = v1.to_var();
+        self.state.assign(x, *v2.clone());
+        debug!("done assigning {:?}", self.state.mem);
+        *v2.clone()
+      },
+      Ternary(ref v1, ref e2, ref e3) if v1.is_value() => {
+        match v1.to_bool() {
+          true => *e2.clone(),
+          false => *e3.clone(),
         }
-      }).collect();
+      },
+      Decl(DConst, ref x, ref v1, ref e2) if v1.is_value() => {
+        subst(*e2.clone(), *x.clone(), *v1.clone())
+      },
+      Decl(DVar, ref x, ref v1, ref e2) if x.is_var() && v1.is_value() => {
+        debug!("allocing {:?}", v1);
+        self.state.alloc(x.to_var(), *v1.clone());
+        Scope(Box::new(*e2.clone()), x.to_var())
+      },
+      Scope(ref v1, ref x) if v1.is_value() => {
+        debug!("freeing {:?}", x);
+        self.state.free(x.clone());
+        *v1.clone()
+      },
+      // lambda lift so we can use iter() in guard
+      // https://github.com/rust-lang/rfcs/issues/1006
+      FnCall(ref v1, ref es) if v1.is_func() && (|| es.iter().all(|v| v.is_value()))() => {
+        match **v1 {
+          Func(ref name, ref e1, ref xs) => {
+            // sub the params
+            let exp = xs.iter().zip(es.iter())
+              .fold(*e1.clone(), |exp, (xn, en)| subst(exp, xn.clone(), en.clone()));
 
-      FnCall(v1.clone(), args2)
-    },
-    FnCall(e1, args) => {
-      FnCall(Box::new(st_step(&mut state, &*e1)), args)
-    },
-  };
+            // sub the fn body for named functions
+            match *name {
+              None => exp,
+              Some(ref s) => subst(exp, *s.clone(), *v1.clone())
+            }
+          },
+          _ => {
+            debug!("expected a Func, got {:?}", v1);
+            panic!()
+          },
+        }
+      },
+      /**
+       * Search Cases
+       */
+      Bop(ref op, ref v1, ref e2) if v1.is_value() => {
+        Bop(
+          op.clone(),
+          Box::new(*v1.clone()),
+          Box::new(self.st_step(e2))
+          )
+      },
+      Bop(Assign, ref v1, ref e2) if v1.is_var() => {
+        Bop(
+          Assign,
+          Box::new(*v1.clone()),
+          Box::new(self.st_step(e2))
+          )
+      },
+      Bop(op, e1, e2) => {
+        Bop(op, Box::new(self.st_step(&*e1)), e2)
+      },
+      Scope(e1, addr) => {
+        Scope(Box::new(self.st_step(&*e1)), addr)
+      },
+      Uop(op, e1) => {
+        Uop(op, Box::new(self.st_step(&*e1)))
+      },
+      Ternary(e1, e2, e3) => {
+        Ternary(Box::new(self.st_step(&*e1)), e2, e3)
+      },
+      While(ref v1, ref e1o, _, ref e2o, ref e3) if v1.is_value() => {
+        match v1.to_bool() {
+          true => While(Box::new(*e1o.clone()), e1o.clone(), e2o.clone(), e2o.clone(), e3.clone()),
+          false => *e3.clone(),
+        }
+      },
+      While(ref e1, ref e1o, ref v2, ref e2o, ref e3) if v2.is_value() => {
+        While(Box::new(self.st_step(&*e1)), e1o.clone(), v2.clone(), e2o.clone(), e3.clone())
+      },
+      While(e1, e1o, e2, e2o, e3) => {
+        While(e1, e1o, Box::new(self.st_step(&*e2)), e2o, e3)
+      },
+      Decl(dt, addr, e1, e2) => {
+        Decl(dt, Box::new(*addr.clone()), Box::new(self.st_step(&*e1)), e2)
+      },
+      FnCall(ref v1, ref args) if v1.is_value() => {
+        let mut found_nonvalue = false;
 
-  debug!("returning with mem {:?}" , state.mem);
-  debug!("returning with e {:?}" , e1);
-  state.with(e1)
+        let args2 = args.iter().map(|e| {
+          if !found_nonvalue && !e.is_value() {
+            found_nonvalue = true;
+            self.st_step(e)
+          } else {
+            e.clone()
+          }
+        }).collect();
+
+        FnCall(v1.clone(), args2)
+      },
+      FnCall(e1, args) => {
+        FnCall(Box::new(self.st_step(&*e1)), args)
+      },
+    };
+
+    debug!("returning with mem {:?}" , self.state.mem);
+    debug!("returning with e {:?}" , e1);
+    self.state.with(e1)
+  }
+
+  pub fn eval(&mut self) -> Expr {
+    let mut num_iterations = 0;
+
+    loop {
+      if num_iterations > 500 {
+        panic!("too many step iterations");
+      }
+
+      debug!("-----------------");
+      debug!("--- iterating on e {:?} ", self.state.expr);
+      debug!("--- iterating on m {:?} ", self.state.mem);
+      num_iterations += 1;
+      if self.state.expr.is_value() {
+        debug!("--- iterations: {}", num_iterations);
+        return self.state.expr.clone()
+      } else {
+        let e = self.state.expr.clone();
+        self.state = self.step(e);
+      }
+    }
+  }
 }
 
 pub fn boxx(input: &str) -> Expr {
-  let mut state = State::from(parse(input));
-
-  let mut num_iterations = 0;
-
-  loop {
-    if num_iterations > 500 {
-      panic!("too many step iterations");
-    }
-
-    debug!("-----------------");
-    debug!("--- iterating on e {:?} ", state.expr);
-    debug!("--- iterating on m {:?} ", state.mem);
-    num_iterations += 1;
-    if state.expr.is_value() {
-      debug!("--- iterations: {}", num_iterations);
-      return state.expr
-    } else {
-      state = step(state);
-    }
-  }
+  let mut repl = Repl::new(parse(input));
+  repl.eval()
 }
