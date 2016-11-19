b144f5370f2b0b2a0d6ab063ff849d68b1b9e2b6
michael <mpgarate@gmail.com>
2016-11-19 13:23:45 -0500

    use meowth name in prompt and hide undefined values

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate boxx;
 
 use boxx::interpreter::Interpreter;
+use boxx::expr::Expr;
 
 use std::io::{Write, stdout, stdin};
 
@@ -8,19 +9,20 @@ fn main() {
   let mut interpreter = Interpreter::new();
 
   loop {
-    print!("boxx> ");
+    print!("meowth :: ");
     let _ = stdout().flush();
 
     let mut input = String::new();
     match stdin().read_line(&mut input) {
       Ok(_) => {
-        if input == "exit\n".to_string() { 
+        if input == String::from("exit\n") {
           break;
         }
 
         let expr_result = interpreter.eval(&input);
         
         match expr_result {
+          Ok(Expr::Undefined) => (),
           Ok(exp) => println!("{:?}", exp), 
           Err(err) => println!("Error: {}", err),
         }
